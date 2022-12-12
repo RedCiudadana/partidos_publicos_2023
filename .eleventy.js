6734b8a255b842e145f7fab26f10a3d2ae8505ef
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-12-12 09:54:09 -0600

    listado de partidos finalizado

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,21 @@ module.exports = function (eleventyConfig) {
         return array.slice(0, limit);
     });
 
-    eleventyConfig.addFilter("uppercase", function(text) {
-        return text.toLowerCase()
-    });
+    eleventyConfig.addFilter("capitalize", function(text) {
+        str = text.toLowerCase()
+        const arr = str.split(" ");
+
+        //loop through each element of the array and capitalize the first letter.
+
+
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+        }
+
+        //Join all the elements of the array back into a string 
+        //using a blankspace as a separator 
+        const str2 = arr.join(" ");
+        return str2 
+        });
 }
