e49fbb42bf63c33e76cd93f043aece15cbbc8c9d
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-12-12 09:44:56 -0600

    trabajo en partidos

--- .eleventy.js
+++ .eleventy.js
@@ -7,11 +7,17 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src');
     eleventyConfig.addPassthroughCopy('admin');
 
+    /* Filtros */
+
     eleventyConfig.addNunjucksFilter("rmj", function(content) {
         return rmj(content);
     });
 
     eleventyConfig.addNunjucksFilter("limit", function (array, limit) {
         return array.slice(0, limit);
+    });
+
+    eleventyConfig.addFilter("uppercase", function(text) {
+        return text.toLowerCase()
     });
 }
