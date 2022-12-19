1c47e410782884e426fc77d6d378fe2ddeebc8e1
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-12-19 13:02:47 -0600

    arreglo de carpetas y orden

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('src');
     eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy('assets');
 
     /* Filtros */
 
